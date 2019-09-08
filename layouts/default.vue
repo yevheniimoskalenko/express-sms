48194f3977cfb39a381ac7eb869b04c881bab29c
Yevhenii Moskalenko <testjokerqwerty@gmail.com>
2019-09-08 22:41:05 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-container>
+    <!-- <el-header>Header</el-header> -->
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+      <el-form ref="form" v-model="form" :inline="true">
+        <el-form-item label="Service">
+          <el-input v-model="form.service" />
+        </el-form-item>
+        <el-button type="primary" :loading="loading" round>Поиск</el-button>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      form: {
+        service: ""
+      }
+    };
+  }
+};
+</script>
